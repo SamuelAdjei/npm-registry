1a74a274d40cc4f972be4bd561a6fddfc04522fc
SamuelAdjei <20958360+SamuelAdjei@users.noreply.github.com>
2021-02-15 16:23:26 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.createApp = void 0;
+var express = require("express");
+var package_1 = require("./package");
+/**
+ * Bootstrap the application framework
+ */
+function createApp() {
+    var app = express();
+    app.use(express.json());
+    app.get('/package/:name', package_1.getPackage);
+    app.get('/package/:name/:version', package_1.getPackage);
+    return app;
+}
+exports.createApp = createApp;
+//# sourceMappingURL=app.js.map
